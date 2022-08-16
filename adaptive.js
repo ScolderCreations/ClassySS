f1c8539e08edb27032670cedbc316de999cf18ca
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-08-16 21:17:24 +0000

    added overflow and display options

--- adaptive.js
+++ adaptive.js
@@ -2,23 +2,23 @@ if (typeof document == "undefined") {
   throw new Error("This script is meant to be run in an HTML page.")
 }
 
-const pEl = document.getElementsByTagName("p")
 const elements = document.querySelectorAll("*")
 
+function keyword(regex, e) {
+  if (element.className.match(regex)) {
+    e()
+  }
+}
+
 for (element of elements) {
-  if (element.target == "_blank") {
-    e.target.innerText = "⇱ " + e.target.innerText
-  }
-  if (element.className.match(/size:([0-9]*\.?[0-9]*?[icmp%ve][mxtnhw]?)/i)) {
-    element.style["font-size"] = RegExp.lastParen;
-  }
-  if (element.className.match(/h:([0-9]*\.?[0-9]*?[icmp%ve][mxtnhw]?)/i)) {
-    element.style["height"] = RegExp.lastParen;
-  }
+  keyword(/size:([0-9]*\.?[0-9]*?[icmp%ve][mxtnhw]?)/i,
+    () => {element.style["font-size"] = RegExp.lastParen;})
+  keyword(/h:([0-9]*\.?[0-9]*?[icmp%ve][mxtnhw]?)/i,
+    () => {element.style["height"] = RegExp.lastParen;})
   if (element.className.match(/w:([0-9]*\.?[0-9]*?[icmp%ve][mxtnhw]?)/i)) {
     element.style["width"] = RegExp.lastParen;
   }
-  if (element.className.match(/bgurl:(.+)/i)) {
+  if (element.className.match(/bgurl:([A-Za-z0-9\.\/\\\?=\+%@&]+)/i)) {
     element.style["background-image"] = "url('"+RegExp.lastParen+"')"
   }
   if (element.className.match(/c:([0-9a-zA-Z\(\),]+)/i)) {
@@ -30,4 +30,6 @@ for (element of elements) {
   if (element.className.match(/wrap:(hide|word|all)/i)) {
     element.style["overflow"] = {hide: "hidden", word: "break-word", all: "break-all"}[RegExp.lastParen];
   }
+  keyword(/show:(b|i|ib|f|if|g|ig|r|no|in)/, // block, inline, inline block, flex, inline flex, grid, inline grid, flex root, none, inherit
+  () => {element.style["display"] = {b: 'block',i:'inline',ib:'inline-block',f:'flex',if:'inline-flex',g:'grid',ig:'inline-grid',r:'flex-root',no:'none','in':'inherit'}[RegExp.lastParen]})
 }
